9bce46dae1a75ffe71fb9afd28cc719881bd00f9
Berkan Karayel <berkankarayel.147@gmail.com>
2025-02-16 21:58:00 +0300

    view

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
 
 namespace MvcCv.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
-        public ActionResult Index()
+		// GET: Default
+
+        // Hakkımda 
+		DbCvEntities1 db = new DbCvEntities1();
+		public ActionResult Index()
         {
-            return View();
+           var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
         }
-    }
+
+
+        // Deneyimlerim 
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+
+        // Eğitimlerim 
+		public PartialViewResult Egitimlerim()
+		{
+			var egitimler = db.TblEgitim.ToList();
+			return PartialView(egitimler);
+		}
+
+
+
+	}
 }
