873f77d44637eb765bd53bbd0e66a6a97763040d
Berkan Karayel <berkankarayel.147@gmail.com>
2025-02-20 01:39:48 +0300

    Yeteneklerim Crud İşlemleri

---
+++ MvcCv/Controllers/YetenekController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        // GET: Yetenek
+        // Entity yukarıda çağırdık. Burada db değişkene atadık bu sayede kullanbilecez.
+     
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetnekler = repo.List();
+            return View(yetnekler);
+        }
+    }
+}
